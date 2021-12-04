712ee1d33a67532283663bf9e2e5308cfae571f9
David Hoover <davehoover1985@gmail.com>
2021-12-04 10:34:14 -0600

    Working with two async test methods being awaited, returning values from the Tasks and the async method, and using a Task.WaitAll() call.

---
+++ AsyncTest/AsyncTester.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncTest
+{
+    public class AsyncTester
+    {
+        public Task<int> PrintEvens()
+        {
+            /*
+             * Notice how this method contains a task definition with a return statement and a return statement for the method
+             * The method returns type Task -- the work being done in this method gets defined as a Task (Task t1)...
+             * ...The task returns a value of type int -- this is set where t1 is defined as "Task<int> t1"
+             * Using "Task t1" would imply a void return type for the task!!
+             * The return value of the task is called its "result". It's treated just like a return value that the "await PrintEvens()" resolves to.
+             * The method's Task return type is returned here using "return t1". This hands off the tasks handle to the await statement which then resolves to the Task's return value once complete.
+             */
+            Task<int> t1 = Task.Factory.StartNew(() =>
+            {
+                int i;
+                for (i = 0; i < 1000; i += 2)
+                {
+                    Console.WriteLine(i);
+                }
+                return i;
+            });
+            return t1;
+        }
+
+        public Task<int> PrintOdds()
+        {
+            Task<int> t1 = Task.Factory.StartNew(() =>
+            {
+                int i;
+                for (i = 1; i < 1000; i += 2)
+                {
+                    Console.WriteLine(i);
+                }
+                return i;
+            });
+            return t1;
+        }
+        
+    }
+}
--- AsyncTest/Program.cs
+++ AsyncTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace AsyncTest
 {
@@ -7,6 +8,32 @@ namespace AsyncTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //RunTests is an async method, so calling it here does not block the main thread
+            Task.WaitAll(RunTests());     //This line runs an async thread - so control continues on from here while the await lines in RunTests() run asynchronously 
+            
+            //!!!The non-await lines seem to run on the main thread? Or at least execute as though they do.
+            //!!!Using Task.WaitAll() or Task.WaitAny() is like thread.join() -- it blocks the calling thread until the task completes
+            Console.WriteLine("Last Main Line");
+            Console.Read();
+        }
+        private static async Task RunTests()
+        {
+            //Use async method for methods you want to run as though they're on a separate thread.
+            //Merge threads back together by returning as Task type and using Tasks.WaitAll()
+            //Returning Task is rather odd - there is NO return statement in this method, yet I am returning Task
+            //Returning void works, but then you have no handle for the thread to return to use Task.WaitAll() with
+            
+            AsyncTester tester = new AsyncTester();
+            // await keyword can be placed before Task objects to run them on a threadpool thread
+            // the async keyword on a method allows for the use of the await keyword
+            // the async method - with or without await keywords - will internally run synchronously i.e. commands in series within the method
+            // outside the async method, the async method caller runs its await statements (and anything after them) asynchronously
+            Console.WriteLine("Is this synchronous?");  //This runs before "Last Main Line" -- implying it runs on the main thread
+            int testNumber = await tester.PrintEvens();      //Await has to await a task -- so here, PrintEvens() returns a Task object
+            //Await requires an "awaitable" -- as far as I can tell this is just a fancy way to say you have to use a Task with it.
+            int testNumber2 = await tester.PrintOdds();     //After printing evens, this method moves synchronously onto printing odds
+            Console.WriteLine("Last run tests line");   //this line executes after PrintOdds() -- basically you're queueing up things synchronously in a separate thread than the thread in which RunTests() was called
+
         }
     }
 }
